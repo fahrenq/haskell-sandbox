6545c0fb3353fe89dfe113be97ecf02759d3b5bf
fahrenq <ivankuznetsov316@gmail.com>
2020-04-14 17:40:22 +0200

    Fixed factorial functions benchmark

--- Factorial.hs
+++ Factorial.hs
@@ -15,14 +15,13 @@ fac'' :: (Integral a) => a -> a -> a
 fac'' 0 acc = acc
 fac'' n acc = fac'' (n - 1) $! n * acc
 
-i = 10000000000
+i = 10000000
 
 main = defaultMain
   [ bgroup
       ("fac of " ++ show i)
-      [ --bench "fac basic" $ whnf fac i
-        bench "fac with acc" $ whnf fac' i
-      , bench "fac with acc $!" $ whnf fac'' i
+      [ -- bench "fac basic" $ whnf fac i
+        bench "fac with acc" $ whnf (\() -> fac' i 1) ()
+      , bench "fac with acc $!" $ whnf (\() -> fac'' i 1) ()
       ]
   ]
-
